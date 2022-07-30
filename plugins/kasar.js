6961eafb9fff08b68c4ecdcd770bf7c7c19a1a73
Sendalgtps <100185355+Sendalgtps@users.noreply.github.com>
2022-07-30 09:11:14 +0700

    Update kasar.js

--- plugins/kasar.js
+++ plugins/kasar.js
@@ -7,7 +7,7 @@ conn.sendFile(m.chat, helloarie, '', '', m, true)
 // await conn.sendMessage(m.chat, { audio: { url: helloarie }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|basta)$/i
+handler.customPrefix = /^(anj|ajg|anjing|bajingan|bngsat|bangsat|kontol|memeq|babi|pepeq|pepek|meki|titid|Tityd|peler|tetek|toket|ngewe|goblok|tolol|ngentod|jembut|bego|dajjal|jancuk|pantek|puki|kimak|kampang|lonte|pelacur|nigga|fuck|dick|bitch|tits|basta)$/i
 handler.command = new RegExp
 
 handler.limit = true
